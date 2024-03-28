3ecf08fd90d8cac6b4dab83405b1ac61c4d3ab39
meetofleaf <meetofleaf@gmail.com>
2024-03-28 18:12:22 -0400

    Updated imports

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -1,7 +1,6 @@
 # Import libraries
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
